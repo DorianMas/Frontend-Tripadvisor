094cdb895c183a1532cf575a3c86ac75a7c4eb00
Dorian Masquelier <dorian.masquelier@gmail.com>
2022-01-29 15:37:09 +0100

    seconde version

--- assets/js/vanilla.js
+++ assets/js/vanilla.js
@@ -3,12 +3,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const openModal = document.getElementById("connexion");
 
+  // Ouvrir le modal relatif au formulaire de contact
+
   openModal.addEventListener("click", () => {
     document.getElementById("modal").classList.add("display");
   });
 
+  // Fermer le modal relatif au formulaire de contact
+
   const closeModal = document.getElementById("closeButton");
   closeModal.addEventListener("click", () => {
-    document.getElementById("modal").classList.add("hidden");
+    document.getElementById("modal").classList.remove("display");
   });
+
+  // Déclencher l'envoi des informations ajoutées dans le formulaire de contact
+  document
+    .querySelector("#contact-form")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const data = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        description: document.querySelector("#description").value,
+      };
+      /* console.log(data); */
+      const response = await axios.post("/form", data);
+      console.log(response);
+    });
 });
